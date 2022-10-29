96ca833be262702e0fa92bec1c79be1559c2b1b9
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2022-10-29 18:53:17 +0300

    Added accessor attributers for article

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,7 @@ require "pry"
 require_relative "./author.rb"
 require_relative "./magazine.rb"
 class Article
-
+    attr_accessor :title, :author, :magazine
     def initialize(title, author, magazine , magazine_category)
         @title = title
         @author = Author.new(author)
